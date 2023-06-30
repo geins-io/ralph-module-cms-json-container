36fb5f3f1710f18bbd538780c9a9cbac13d55b2e
arvidsson-geins <arvidsson@geins.io>
2023-06-30 13:44:46 +0200

    init

---
+++ lib/components/molecules/GeinsWidgetJsonContainer/GeinsWidgetJsonContainer.vue
@@ -0,0 +1,47 @@
+<template>
+  <component :is="widgetType" v-if="widgetType" :configuration="widgetData" />
+</template>
+<script>
+/* 
+  (Description of component) 
+*/
+export default {
+  name: 'GeinsWidgetJsonContainer',
+  components: {},
+  mixins: [],
+  props: {
+    // Widget configuration object
+    configuration: {
+      type: Object,
+      required: true
+    }
+  },
+  data: () => ({}),
+  computed: {
+    // Get the widget type from the configuration object
+    widgetType() {
+      if (this.confObject) {
+        return this.confObject.renderWidget;
+      }
+      return '';
+    },
+    widgetData() {
+      /*if (this.widgetType === 'CaWidgetStreamify'|| this.widgetType === 'GeinsWidgetStreamify') {
+        return this.confObject.data;
+      }*/
+      return this.confObject.data ?? {};
+    },
+    confObject() {
+      if (this.configuration?.json) {
+        return JSON.parse(this.configuration?.json);
+      }
+      return {};
+    }
+  },
+  watch: {},
+  mounted() {
+    console.log('GeinsWidgetJsonContainer mounted');
+  },
+  methods: {}
+};
+</script>
